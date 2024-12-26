9788758cd0a28c294b799d074dbe260c4d8914a4
Sibi Siddharth S <sibisiddharth008@gmail.com>
2024-12-26 17:02:56 +0530

    minor mobile oprimization

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -24,7 +24,7 @@ const Display = () => {
   })
 
   return (
-    <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+    <div ref={displayRef} className='w-[100%] m-2 px-4 sm:px-10 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
       {albumsData.length > 0 
       ?
       <Routes>
--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,64 +1,80 @@
-import React, { useContext, useState, useEffect } from 'react'
-import Navbar from './Navbar'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useState, useEffect } from 'react';
+import Navbar from './Navbar';
+import { useParams } from 'react-router-dom';
 import { assets } from '../assets/assets';
 import { PlayerContext } from '../context/PlayerContext';
 
-const DisplayAlbum = ({album}) => {
+const DisplayAlbum = ({ album }) => {
+  const { id } = useParams();
+  const [albumData, setAlbumData] = useState('');
+  const { playWithId, albumsData, songsData } = useContext(PlayerContext);
 
-    const { id } = useParams();
-    const [albumData, setAlbumData] = useState('');
-    const {playWithId, albumsData, songsData} = useContext(PlayerContext);
+  useEffect(() => {
+    albumsData.map((item) => {
+      if (item._id === id) {
+        setAlbumData(item);
+      }
+    });
+  }, []);
 
-    useEffect(() => {
-      albumsData.map((item)=>{
-        if (item._id === id){
-          setAlbumData(item);
-        }
-      })
-    }, []);
+  const albumSongs = songsData.filter(
+    (item) => item.album.toLowerCase() === albumData.name?.toLowerCase()
+  );
+
+  const totalDuration = albumSongs.reduce((acc, song) => {
+    const [minutes, seconds] = song.duration.split(':').map(Number);
+    return acc + minutes * 60 + seconds; 
+  }, 0);
+
+  const hours = Math.floor(totalDuration / 3600);
+  const minutes = Math.floor((totalDuration % 3600) / 60);
+
+  const displayDuration = `${hours > 0 ? `${hours} hr` : ''} ${minutes} min`;
 
   return albumData ? (
     <>
-      <Navbar/>
-      <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
-        <img className='w-48 rounded' src={albumData.image} alt="" />
-        <div className='flex flex-col'>
-            <p>Playlist</p>
-            <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
-            <h4>{albumData.desc}</h4>
-            <p>
-                <img className='inline-block w-5 m-1' src={assets.spotify_logo} alt="" />
-                <b className='text-[14px]' >SpotGPT </b>
-                • 9,129,199 likes
-                • <b>50 songs, </b>
-                about 2 hr 30 min
-            </p>
+      <Navbar />
+      <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-end">
+        <img className="w-48 rounded" src={albumData.image} alt="" />
+        <div className="flex flex-col">
+          <p>Playlist</p>
+          <h2 className="text-5xl font-bold mb-4 md:text-7xl">{albumData.name}</h2>
+          <h4>{albumData.desc}</h4>
+          <p>
+            <img className="inline-block w-5 m-1" src={assets.spotify_logo} alt="" />
+            <b className="text-[14px]">SpotGPT • <span className='text-[14px] font-semibold'>1084 likes</span></b> 
+          </p>
+          <b className="text-[14px] font-medium m-1">{albumSongs.length} songs, about {displayDuration}</b> 
         </div>
       </div>
-      <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
-        <p><b className='mr-4'>#</b>Title</p>
-        <p>Album</p>
-        <p className='hidden sm:block'>Date Added</p>
-        <img className='m-auto w-4' src={assets.clock_icon} alt="" />
+      <div className="grid grid-cols-[2fr_1fr] sm:grid-cols-3 mt-8 sm:mt-10 mb-4 pl-2 text-[#a7a7a7]">
+        <p>
+          <b className="mr-4">#</b>
+          <b className="m-auto">Title</b>
+        </p>
+  
+        <p className="hidden sm:block">Date Added</p>
+        <img className="m-auto w-4" src={assets.clock_icon} alt="" />
       </div>
-      <hr />
-      {
-        songsData.filter((item) => item.album.toLowerCase() === albumData.name.toLowerCase()).map((item, index) => (
-            <div onClick={()=> playWithId(item._id)} key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
-                <p className='text-white'>
-                    <b className='mr-4 text-[#a7a7a7]'>{index+1}</b>
-                    <img className='inline w-10 mr-5' src={item.image} alt="" />
-                    {item.name}
-                </p>
-                <p className='text-[15px]'>{albumData.name}</p>
-                <p className='text-[15px] hidden sm:block'>5 days ago</p>
-                <p className='text-[15px] text-center'>{item.duration}</p>
-            </div>
-        ))
-      }
+      <hr className='mb-2'/>
+      {albumSongs.map((item, index) => (
+        <div
+          onClick={() => playWithId(item._id)}
+          key={index}
+          className="grid grid-cols-[2fr_1fr] sm:grid-cols-3 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
+        >
+          <p className="text-white w-[23ch] truncate sm:w-full">
+            <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
+            <img className="inline w-10 h-10 mr-5" src={item.image} alt="" />
+            {item.name}
+          </p>
+ 
+          <p className="text-[15px] hidden sm:block">5 days ago</p>
+          <p className="text-[15px] text-center">{item.duration}</p>
+        </div>
+      ))}
     </>
   ) : null;
-}
+};
 
-export default DisplayAlbum
+export default DisplayAlbum;
