27bdb97a015768e38e40121d6cebb00ad1c720d7
Sibi Siddharth S <sibisiddharth008@gmail.com>
2025-01-02 02:13:45 +0530

    changed description length before truncate

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -16,7 +16,7 @@ const AlbumItem = ({ image, name, desc, id }) => {
       <img className='h-14 object-cover rounded-l-md ' src={image} alt="" />
       <div className='flex flex-col'>
         <p className='font-bold px-2.5 sm:px-4 text-[12px] sm:text-[14px]'>{name}</p>
-        <p className='font-normal text-[12px] hidden sm:block px-4 w-[25ch] truncate'>{desc}</p>
+        <p className='font-normal text-[12px] hidden sm:block px-4 w-[35ch] truncate'>{desc}</p>
       </div>
     </div>
   );
