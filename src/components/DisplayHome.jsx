931edf0796838342dbac6fa31e50eadd97f7d110
Sibi Siddharth S <sibisiddharth008@gmail.com>
2025-01-04 22:29:28 +0530

    superai text added

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -12,7 +12,7 @@ const DisplayHome = () => {
     let shuffledArray = [...array]; 
     for (let i = shuffledArray.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; // Swap elements
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
     }
     return shuffledArray;
   };
@@ -36,19 +36,22 @@ const DisplayHome = () => {
         </div>
       </div>
       <div className="mb-10">
-      <h1 className="my-5 font-bold text-2xl">Random Song List</h1>
-      <div className="flex overflow-auto">
-        {shuffledSongsData.map((item, index) => (
-          <SongItem
-            key={index}
-            name={item.name}
-            desc={item.desc}
-            id={item._id}
-            image={item.image}
-          />
-        ))}
+        <div className="mt-5">
+          <h1 className="font-bold text-2xl">AI Based Suggestions</h1>
+          <p className="text-xs text-gray-500 italic md:text-end">Powered By SuperAI</p>
+        </div>
+        <div className="flex overflow-auto">
+          {shuffledSongsData.map((item, index) => (
+            <SongItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item._id}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
